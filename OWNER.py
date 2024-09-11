6c60694e2e0a0888518d6fadb6496368d994763b
noo-bot1234 <noornocr@gmail.com>
2024-09-11 20:19:21 +0300

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["ELHYBA"]
+OWNER_NAME = "mody"
+BOT_TOKEN = "6768374873:AAGuOBIEQQ2Hw7xyIlUDQVZk6YgKoehJV6w"
+DATABASE = "mongodb+srv://albolbol:albolbolzpy@cluster0.cpd7rm5.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/Source_Ze"
+GROUP = "https://t.me/ZeSupport"
+VIDEO = "https://telegra.ph/file/7065a4c7cbedef8befdb9.jpg"
+LOGS = "zemaker1"
+
